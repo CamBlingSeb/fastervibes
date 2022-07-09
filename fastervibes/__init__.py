58ce536c5ff70c286a4b4b7ef952fe8191190a9d
Seb <sebastian@camblingcams.com>
2022-07-09 07:08:47 -0600

    fixed old references to flask template name

--- fastervibes/__init__.py
+++ fastervibes/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'fastervibes.sqlite'),
     )
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -21,11 +21,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello World'
 
     from . import db
     db.init_app(app)
